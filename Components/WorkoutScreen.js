675e7c0375d045ad1140c87303097aff88ae2a52
tavice <thomasavice.ta@gmail.com>
2024-03-07 16:01:09 -0800

    working on chronometer

--- Components/WorkoutScreen.js
+++ Components/WorkoutScreen.js
@@ -1,12 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+//import chronometer from "./Stopwatch";
+import Stopwatch from "./Stopwatch";
 
 const WorkoutScreen = ({ route }) => {
   const { exerciseData } = route.params;
   const [currentSet, setCurrentSet] = useState(1);
   const [isResting, setIsResting] = useState(false);
   const [timer, setTimer] = useState(exerciseData.restTime); // Rest time in seconds
-  const [chronometer, setChronometer] = useState(0);
+  const [stopwatchRunning, setStopwatchRunning] = useState(false);
+
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -14,7 +18,7 @@ const WorkoutScreen = ({ route }) => {
         setTimer(timer - 1);
       } else {
         setIsResting(false);
-        setCurrentSet(prevSet => prevSet + 1);
+        setCurrentSet((prevSet) => prevSet + 1);
       }
     }, 1000);
     return () => clearInterval(intervalId);
@@ -25,23 +29,43 @@ const WorkoutScreen = ({ route }) => {
     setTimer(exerciseData.restTime);
   };
 
-  const handleChronometer = () => {
-    setChronometer(chronometer + 1);
+  const handleStartStopwatch = () => {
+    setStopwatchRunning(true);
   };
+
+  const handleStopStopwatch = () => {
+    // Pass the recorded time to a function for storing workout data
+    handleRecordWorkoutTime(stopwatchTime);
+    setStopwatchRunning(false);
+  };
+
+  const handleRecordWorkoutTime = (time) => {
+    // Send the time to an API or store it in a database
+    console.log(`Workout time: ${time} seconds`);
+  };
+
+  
 
   return (
     <View style={styles.container}>
       <Text style={styles.textWorkout}>Exercise: {exerciseData.name}</Text>
-      <Text style={styles.textWorkout}>Set: {currentSet} / {exerciseData.sets}</Text>
+      <Text style={styles.textWorkout}>
+        Set: {currentSet} / {exerciseData.sets}
+      </Text>
+      <Stopwatch 
+       running={stopwatchRunning}
+       onStart={handleStartStopwatch}
+       onStop={handleStopStopwatch}
+      />
+
       {isResting ? (
         <Text style={styles.textWorkout}>Rest: {timer} seconds</Text>
       ) : (
         <>
           <Text style={styles.textWorkout}>Reps: {exerciseData.reps}</Text>
           <Button title="Set Completed" onPress={handleSetCompleted} />
+       
         </>
-
-        
       )}
     </View>
   );
@@ -52,12 +76,11 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
- textWorkout: {
+  textWorkout: {
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
   },
-  
 });
 
 export default WorkoutScreen;
